5898acdab0719a42c3bf90e3a4e0f96a3311724f
Esau21 <esaumoran1999@gmail.com>
2025-07-04 17:16:11 -0600

    aprendiendo Go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var miNombre string = "Edgar Esau Zelaya Moran"
+	var apellidoPersona = "Ayala"
+	tercerNombre := "hector"
+	fmt.Println("Hola mundo mi nombre es :" + miNombre)
+	fmt.Println("Mi apelido es: " + apellidoPersona)
+	fmt.Println("Este es mi otro nombre desde Go :" + tercerNombre)
+}
