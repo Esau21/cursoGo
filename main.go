0b435ab41ec5727ef26036bd34f3ace11f0508dc
Esau21 <esaumoran1999@gmail.com>
2025-07-04 17:25:15 -0600

    aprendiendo Go

--- main.go
+++ main.go
@@ -10,4 +10,13 @@ func main() {
 	fmt.Println("Hola mundo mi nombre es :" + miNombre)
 	fmt.Println("Mi apelido es: " + apellidoPersona)
 	fmt.Println("Este es mi otro nombre desde Go :" + tercerNombre)
+
+	/* Parte numerica para Go */
+
+	var numero1 = 10
+	var numero2 = 10
+
+	var sumarDesdeGo = numero1 + numero2
+
+	fmt.Println(sumarDesdeGo)
 }
