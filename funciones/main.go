ea17a5f8b8a077b415cf9d56472da8112499ea89
Esau21 <esaumoran1999@gmail.com>
2025-07-09 18:49:01 -0600

    commit de go

--- funciones/main.go
+++ funciones/main.go
@@ -1,27 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Bienvenidos a mi calculadora en Golang")
-	var operador string
-	fmt.Println("Escoge un operador por favor: +, -, *, /")
-	fmt.Scan(&operador)
+	fmt.Println("Bienvenidos a mi calculadora")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Por favor escoge un operador para poder hacer la operacion matematica")
+	fmt.Println("Operadores validos, +, -, *, /")
+	fmt.Println("Si  quieres salir del programa solo presiona la tecla enter")
+
+	operadorEnter, _ := reader.ReadString('\n')
+	operador := strings.TrimSpace(operadorEnter)
+
+	if operador == "" {
+		fmt.Println("Saliendo del programa regresa pronto...")
+		return
+	}
 
 	var numero1, numero2 int
-	fmt.Println("Ingresa tus numeros")
+	fmt.Println("Inserta dos numeros para poder reaizar la operacion" + operador)
 	fmt.Scan(&numero1, &numero2)
 
 	switch operador {
 	case "+":
-		fmt.Println(numero1 + numero2)
+		suma := numero1 + numero2
+		fmt.Printf("EL RESULTADO DE ESTA SUMA ES: %d\n", suma)
 	case "-":
-		fmt.Println(numero1 - numero2)
+		resta := numero1 - numero2
+		fmt.Printf("EL RESULTADO DE ESTE RESTA ES: %d\n", resta)
+
 	case "*":
-		fmt.Println(numero1 * numero2)
+		multiplicacion := numero1 * numero2
+		fmt.Printf("EL RESULTADO DE ESTA MULTIPLICACION ES: %d\n", multiplicacion)
 	case "/":
-		fmt.Println(numero1 / numero2)
+		if numero2 != 0 {
+			divison := float64(numero1) / float64(numero2)
+			fmt.Printf("EL RESULTADO DE ESTA DIVISION ES: %.2f\n", divison)
+		} else {
+			fmt.Println("Error no es divisible entre 0")
+		}
 	default:
-		fmt.Println("Operador ingresado invalido")
+		fmt.Println("Error el operador que elegiste no es valido")
 	}
+
 }
